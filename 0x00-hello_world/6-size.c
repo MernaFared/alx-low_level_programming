a7839d6d7e572e580bbf542c45893acc26d8d910
MernaFared <mernafared77@gmail.com>
2022-09-08 18:24:03 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main -main block
+* Return: 0
+*
+*/
+int main(void)
+{
+char c;
+int i;
+long li;
+long long lli;
+float f;
+printf("size of a char: %ld bytes(s)\n", sizeof(c));
+printf("size of an int: %ld bytes(s)\n", sizeof(i));
+printf("size of a long int: %ld bytes(s)\n", sizeof(li));
+printf("size of a long long int: %ld bytes(s)\n", sizeof(lli));
+printf("size of a float: %ld bytes(s)\n", sizeof(f));
+return (0);
+}
